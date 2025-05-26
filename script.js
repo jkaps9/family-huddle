df6f5ca1185e82596dbd736ba2c2cc4a37ce86df
Josh <15219491+jkaps9@users.noreply.github.com>
2025-05-25 23:55:45 -0400

    Style main menu

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('script linked!');
+const DisplayController = (function () {
+    const mainMenu = document.querySelector("#main-menu");
+    const hamburger = document.querySelector("#hamburger");
+    const backButton = document.querySelector("#back");
+
+    hamburger.addEventListener('click', () => toggleMainMenu());
+    backButton.addEventListener('click', () => toggleMainMenu());
+
+    const toggleMainMenu = () => {
+        mainMenu.classList.toggle("hidden");
+    };
+
+})();
